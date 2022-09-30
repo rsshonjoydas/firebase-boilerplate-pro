7b17d45e86d9fe446fdd2969c8a3e1eca96be89b
”rsshonjoydas” <rsshonjoydas@email.com>
2022-09-30 18:25:36 +0600

    refactor: dashboard sidebar logout

--- components/Layout/Dashboard/Sidebar.tsx
+++ components/Layout/Dashboard/Sidebar.tsx
@@ -1,6 +1,9 @@
 import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouter } from 'next/router';
 import { sidebarData } from '../../../adapters/data/NavbarData';
+import { useAppDispatch } from '../../../redux/hook';
+import { logout } from '../../../redux/reducer/authReducer';
 
 import NavbarItem from '../NavbarItem';
 
@@ -13,24 +16,31 @@ const SidebarPowerIcon = ({ icon, text }: any) => (
 
 export const Divider = () => <hr className='sidebar-hr' />;
 
-const Sidebar = () => (
-  <div className='fixed top-0 left-0 flex flex-col w-16 h-screen bg-white shadow-lg dark:bg-gray-900'>
-    {sidebarData.map((menuItem) => (
-      <span key={menuItem.id}>
-        <NavbarItem href={`/dashboard/${menuItem.path}`} className='sidebar sidebar-icon group'>
-          <FontAwesomeIcon icon={menuItem.icon} className='h-5' />
-          <span className='tooltip tooltip-left'>{menuItem.tooltip}</span>
-        </NavbarItem>
-        {menuItem.tooltip === 'Home' || menuItem.tooltip === 'Profile' ? <Divider /> : null}
-      </span>
-    ))}
-    <div className='absolute inset-x-0 bottom-0 h-16 ...'>
-      <SidebarPowerIcon
-        icon={<FontAwesomeIcon icon={faPowerOff} className='h-6' />}
-        text='Log out'
-      />
+const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  return (
+    <div className='fixed top-0 left-0 flex flex-col w-16 h-screen bg-white shadow-lg dark:bg-gray-900'>
+      {sidebarData.map((menuItem) => (
+        <span key={menuItem.id}>
+          <NavbarItem href={`/dashboard/${menuItem.path}`} className='sidebar sidebar-icon group'>
+            <FontAwesomeIcon icon={menuItem.icon} className='h-5' />
+            <span className='tooltip tooltip-left'>{menuItem.tooltip}</span>
+          </NavbarItem>
+          {menuItem.tooltip === 'Home' || menuItem.tooltip === 'Profile' ? <Divider /> : null}
+        </span>
+      ))}
+      <div className='absolute inset-x-0 bottom-0 ml-2 h-16 ...'>
+        <button type='submit' onClick={() => dispatch(logout(router))}>
+          <SidebarPowerIcon
+            icon={<FontAwesomeIcon icon={faPowerOff} className='h-6' />}
+            text='Log out'
+          />
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Sidebar;
